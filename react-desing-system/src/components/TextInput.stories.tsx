608c26fd8464a55433f3da6441d732ce2e5e9cd3
Prg-maker <df3296709@gmail.com>
2022-10-14 14:54:52 -0300

    add textInput customize

--- react-desing-system/src/components/TextInput.stories.tsx
+++ react-desing-system/src/components/TextInput.stories.tsx
@@ -1,13 +1,23 @@
-import {TextInput , TextInputInputProps, TextInputRootProps} from './TextInput'
+import {TextInput , TextInputRootProps} from './TextInput'
 import {Meta, StoryObj} from '@storybook/react'
+
+import {Envelope} from 'phosphor-react'
 
 export default {
   title:'Components/TextInput',
   component:TextInput.Root,
   args:{
     children:(
+      <>
+
+      <TextInput.Icon>
+        <Envelope/>
+      </TextInput.Icon>
       <TextInput.Input placeholder='type your e-mail address'/>
+        
+      </>
     )
+
   },
   argTypes: {
  
--- react-desing-system/src/components/TextInput.tsx
+++ react-desing-system/src/components/TextInput.tsx
@@ -1,6 +1,5 @@
+import { Slot } from "@radix-ui/react-slot";
 import React, { InputHTMLAttributes } from "react";
-import {clsx} from 'clsx'
-
 
 export interface TextInputRootProps{
   children: React.ReactNode
@@ -13,13 +12,21 @@ function TextInputRoot({children}:TextInputRootProps){
     </div>
   )
 }
+TextInputRoot.displayName = 'TextInput.Root'
 
+export interface TextInputIconProps{
+  children: React.ReactNode,
+}
 
-/*function TextInputIcon(){
-  return()
+function TextInputIcon({children}:TextInputIconProps){
+  return(
+    <Slot className="w-6 h-6 text-gray-400 " >
+      {children}
+    </Slot>
+  )
+}
 
-}*/
-
+TextInputIcon.displayName = 'TextInput.Icon'
 
 export interface TextInputInputProps  extends InputHTMLAttributes<HTMLInputElement>{}
 
@@ -31,9 +38,11 @@ function TextInputInput(props: TextInputInputProps) {
 
 )
 }
+TextInputInput.displayName = 'TextInput.TextInputInput'
+
 
 export const TextInput={
   Root:TextInputRoot,
   Input: TextInputInput,
-
+  Icon:TextInputIcon
 } 
